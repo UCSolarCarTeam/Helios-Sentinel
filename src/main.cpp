4183b0e7f74301da0fdb1a1d0cd39710f7b85248
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2024-05-29 13:54:29 -0600

    added motorFaults to main to be forwarded

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "import_qml_plugins.h"
 
 #include "./SerialPortForwarder.h"
+#include "MotorFaults.h"
 #include "lights.h"
 #include "DriverControls.h"
 
@@ -21,6 +22,9 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    MotorFaults motorFaults;
+    engine.rootContext()->setContextProperty("motorFaults", &motorFaults);
 
     Lights lights;
     engine.rootContext()->setContextProperty("lights", &lights);
@@ -51,6 +55,7 @@ int main(int argc, char *argv[])
     bool firstRun = true;
     QObject::connect(&timer, &QTimer::timeout, [&forwarder, &lights, &driverControls]() {
 
+        forwarder.forwardData(motorFaults.encodedByteStream());
         forwarder.forwardData(lights.encodedByteStream());
         forwarder.forwardData(driverControls.encodedByteStream());
         // forwarder.forwardData(driverControls.byteStream());
