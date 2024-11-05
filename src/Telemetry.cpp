0c4513ef1bb2e0f77ff31ad62ce152ad81d722f8
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2024-11-04 18:27:35 -0700

    Intialized header/source file

---
+++ src/Telemetry.cpp
@@ -0,0 +1,3 @@
+#include "Telemetry.h"
+
+Telemetry::Telemetry() {}
---
+++ src/Telemetry.h
@@ -0,0 +1,10 @@
+#ifndef TELEMETRY_H
+#define TELEMETRY_H
+
+class Telemetry
+{
+public:
+    Telemetry();
+};
+
+#endif // TELEMETRY_H
