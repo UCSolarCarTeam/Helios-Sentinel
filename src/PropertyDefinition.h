de836dd69e7af1f8103286dfcdf3afa57e46434f
jtran217 <jktran1998@gmail.com>
2025-01-11 15:24:00 -0700

    Moved all macros into one header. Defining different macro for int,float and boolean

--- src/PropertyDefinition.h
+++ src/PropertyDefinition.h
@@ -1,9 +1,10 @@
 #ifndef PROPERTYDEFINITION_H
 #define PROPERTYDEFINITION_H
 #include <QObject>
+#include "util.h"
 
 /**
- *  Defines Packet Attribute Properties for linking to qml
+ *  Defines Boolean,Int (Unsigned Int),Float  Packet Attribute Properties for linking to qml
  *  Creates Q_PROPERY with getter, setter, and signal, as well as the
  *  private class var (type name_)
  *
@@ -17,19 +18,59 @@
  *  Note that name should be capilized in current state to follow
  *  best naming conventions
 */
-#define DEFINE_PROPERTY(type, name) \
+
+#define DEFINE_PROPERTY_BOOL(name, position, offset) \
 Q_SIGNALS:\
-    void name##Changed(type on);\
+    void name##Changed(bool on);\
 private:\
-    type name##_; \
+    bool name##_ = false; \
 \
 public Q_SLOTS: \
-    void set##name(type on); \
+    void set##name(bool on){\
+            byteStream_[position] += on ? offset:-offset;\
+            name##_ = on; \
+            updateByteStream(); \
+        }; \
 public: \
-type name() const; \
-Q_PROPERTY(bool name READ name WRITE set##name NOTIFY name##Changed); \
+Q_PROPERTY(bool name READ name WRITE set##name NOTIFY name##Changed);\
+bool name() const {return name##_;} \
 
 
+#define DEFINE_PROPERTY_INT(type, name, position, size)\
+Q_SIGNALS:\
+    void name##Changed(type on);\
+    private:\
+    type name##_ = 0; \
+\
+    public Q_SLOTS: \
+    void set##name(type value) { \
+        QByteArray in = Util::formatInt(value,size);\
+        byteStream_.replace(position, size, in);\
+        name##_ = value;\
+        updateByteStream();\
+}\
+    public: \
+    Q_PROPERTY(type name READ name WRITE set##name NOTIFY name##Changed);\
+    type name() const {return name##_;}
+
+
+
+#define DEFINE_PROPERTY_FLOAT(type, name, position, size)\
+Q_SIGNALS:\
+    void name##Changed(type on);\
+    private:\
+    type name##_ = 0.0; \
+\
+    public Q_SLOTS: \
+    void set##name(type value) { \
+        QByteArray in = Util::formatFloat(value);\
+        byteStream_.replace(position, size, in);\
+        name##_ = value;\
+        updateByteStream();\
+}\
+    public: \
+    Q_PROPERTY(type name READ name WRITE set##name NOTIFY name##Changed);\
+    type name() const {return name##_;}
 
 
 #endif // PROPERTYDEFINITION_H
