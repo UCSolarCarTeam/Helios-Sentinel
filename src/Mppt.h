81701c1be4d42f255ed2120751430125d5600c14
Mason Bullock <mason.bullock@ucalgary.ca>
2025-05-27 19:12:47 -0600

    mppt cleanup

--- src/Mppt.h
+++ src/Mppt.h
@@ -9,12 +9,12 @@ class Mppt : public QObject
 public:
     explicit Mppt(int channel, QObject *parent = nullptr);
 
-    DEFINE_BOOL(Alive,2, 0x80)
+    DEFINE_BOOL(Alive, 2, 0x80)
+
     DEFINE_WHOLE_NUMBER(int, ArrayVoltage, 3, 2)
     DEFINE_WHOLE_NUMBER(int, ArrayCurrent, 5, 2)
     DEFINE_WHOLE_NUMBER(int, BatteryVoltage, 7, 2)
     DEFINE_WHOLE_NUMBER(int, Temp, 9, 2)
-
 
     Q_PROPERTY(QString byteStreamStr READ byteStreamStr NOTIFY byteStreamStrChanged FINAL)
     Q_PROPERTY(QString encodedByteStreamStr READ encodedByteStreamStr NOTIFY encodedByteStreamStrChanged FINAL)
@@ -24,20 +24,15 @@ public:
     QString byteStreamStr() const;
     QString encodedByteStreamStr() const;
 
-
-
 signals:
-
     void byteStreamStrChanged();
     void encodedByteStreamStrChanged();
 
 private:
-
     QByteArray byteStream_;
     QByteArray encodedByteStream_;
 
     void updateByteStream();
-
 };
 
 #endif // MPPT_H
