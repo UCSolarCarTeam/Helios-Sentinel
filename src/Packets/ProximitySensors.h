bc431e4eabe119b67480a9992838321208c37fbc
Mason Bullock <mason.bullock@ucalgary.ca>
2025-06-02 16:27:45 -0600

    moved constructor implementation to cpp file

--- src/Packets/ProximitySensors.h
+++ src/Packets/ProximitySensors.h
@@ -18,17 +18,7 @@ class ProximitySensors : public IPacket {
     DEFINE_PROPERTY(unsigned short, ProximitySensor4, 0x703)
 
 public:
-    ProximitySensors(QCanBusDevice* canDevice) : IPacket(canDevice) {
-
-        timerData_ = {
-            {0x700, {500, [this]() { sendProximitySensor1Message(); }}},
-            {0x701, {500, [this]() { sendProximitySensor2Message(); }}},
-            {0x702, {500, [this]() { sendProximitySensor3Message(); }}},
-            {0x703, {500, [this]() { sendProximitySensor4Message(); }}}
-        };
-
-        initAndStartTimers();
-    };
+    ProximitySensors(QCanBusDevice* canDevice);
 };
 
 #endif // PROXIMITYSENSORS_H
